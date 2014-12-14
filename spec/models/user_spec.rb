915063c2eb9d28a92904dd2ad9ba36687ec2714c
Job van der Voort <jobvandervoort@gmail.com>
2014-12-14 23:08:38 +0100

    user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
