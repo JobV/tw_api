ebeaf946f571f28b41d36f5b40a1b80cfc146e92
Marcelo Lebre <marcelo@dwitmee.com>
2014-12-28 00:59:22 +0000

    updating tests for new rest path policy on location

--- spec/api/user_api_spec.rb
+++ spec/api/user_api_spec.rb
@@ -76,3 +76,73 @@ RSpec.describe 'delete /api/v1/users/1', type: :request do
     end
   end
 end
+
+RSpec.describe 'post /api/v1/users/:id/location', type: :request do
+  let(:user) { create(:user) }
+
+  context 'post with all params' do
+    before do
+      post "/api/v1/users/#{user.id}/location",
+      x: '52.440723',
+      y: '4.809657',
+      z: '0',
+      m: '0'
+      #id: "#{user.id}"
+    end
+
+    it 'returns 201' do
+      expect(response.code).to eq '201'
+    end
+
+    it 'returns a message' do
+      expect(response.message).to eq 'Created'
+    end
+
+    it 'creates a new location' do
+      expect(user.locations.count).to eq 1
+      expect(user.locations.last.longlat.x).to eq 52.440723
+      expect(user.locations.last.longlat.y).to eq 4.809657
+      expect(user.locations.last.longlat.z).to eq 0
+      expect(user.locations.last.longlat.m).to eq 0
+    end
+  end
+
+  context 'post with too little params' do
+    before do
+      post "/api/v1/users/#{user.id}/location",
+      x: "11"
+    end
+
+    it 'returns 500' do
+      expect(response.code).to eq '500'
+    end
+
+    it 'returns a vague message' do
+      expect(response.message).to eq 'Internal Server Error'
+    end
+
+    it 'does not create a new location' do
+      expect(user.locations.count).to eq 0
+    end
+  end
+end
+
+RSpec.describe 'get /api/v1/users/:id/location', type: :request do
+  let(:user) { create(:user) }
+  before do
+    user.locations.create(
+    longlat:"POINT(52.440723 4.809657 0 0)")
+    #get '/api/v1/location', id: user.id
+    get "/api/v1/users/#{user.id}/location"
+  end
+
+  it 'returns 200' do
+    expect(response.code).to eq '200'
+  end
+
+  it 'returns the location in json' do
+    json = JSON.parse(response.body)
+    expect(json['x']).to eq 52.440723
+    expect(json['y']).to eq 4.809657
+  end
+end
