c0c91c8bab3992c81ca0a380d08129d20c1541e0
Marcelo Lebre <marcelo@dwitmee.com>
2014-12-28 00:42:37 +0000

    removing duplicate rest resource methods from location_api.rb

--- app/api/v1/location_api.rb
+++ app/api/v1/location_api.rb
@@ -7,38 +7,7 @@ module V1
     helpers V1::GrapeHelper
 
     resource :location do
-      desc "Set the location of the user."
-      params do
-        requires :id, type: Integer, desc: "User id."
-        requires :x, type: String, desc: "X coordinate."
-        requires :y, type: String, desc: "Y coordinate."
-        optional :z, type: String, desc: "Z coordinate."
-        optional :m, type: String, desc: "M coordinate."
-      end
-      post do
-        user.locations.create!(
-          longlat: "POINT(#{params[:x]}
-                          #{params[:y]}
-                          #{params[:z]}
-                          #{params[:m]})")
 
-      end
-
-      desc "Get the last location of the user."
-      params do
-        requires :id, type: Integer, desc: "User id."
-      end
-      route_param :id do
-        get do
-          loc = user.locations.last
-          {
-            "x" => loc.longlat.x,
-            "y" => loc.longlat.y,
-            "z" => loc.longlat.z,
-            "m" => loc.longlat.m
-          }
-        end
-      end
     end
   end
 end
