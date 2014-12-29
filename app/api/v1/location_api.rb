a0f6639f3e925ed4e9b9eb532646ab37f5c12c07
Marcelo Lebre <marcelo@dwitmee.com>
2014-12-29 21:09:18 +0000

    separating location api and updating tests accordingly

--- app/api/v1/location_api.rb
+++ app/api/v1/location_api.rb
@@ -6,8 +6,33 @@ module V1
     rescue_from :all
     helpers V1::GrapeHelper
 
-    resource :location do
+    resource :users do
+      desc "Get the last known location of the user."
+      get ':id/location' do
+        loc = user.locations.last
+        {
+          "x" => loc.longlat.x,
+          "y" => loc.longlat.y,
+          "z" => loc.longlat.z,
+          "m" => loc.longlat.m
+        }
+      end
 
+      desc "Set the location of the user."
+      params do
+        requires :x, type: String, desc: "X coordinate."
+        requires :y, type: String, desc: "Y coordinate."
+        optional :z, type: String, desc: "Z coordinate."
+        optional :m, type: String, desc: "M coordinate."
+      end
+      post ':id/location' do
+        user.locations.create!(
+        longlat: "POINT(#{params[:x]}
+        #{params[:y]}
+        #{params[:z]}
+        #{params[:m]})")
+      end
     end
+
   end
 end
