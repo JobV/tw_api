a5a57afbba1bdaa19e4cc1f91ad093efdd169966
Marcelo Lebre <marcelo@dwitmee.com>
2015-02-11 22:54:19 +0000

    helper cleanup

--- app/helpers/apns_helper.rb
+++ app/helpers/apns_helper.rb
@@ -1,21 +1,26 @@
 module ApnsHelper
 
-  def send_meetup_notification_to user
+  def pusher
     pusher = Grocer.pusher(
       certificate: "config/ck.pem",
       passphrase:  "pushmeetup",
       gateway:     "gateway.sandbox.push.apple.com"
     )
-    
-    device = user.devices.last
+  end
 
-    notification = Grocer::Notification.new(
+  def generate_notification_for device, user, message
+    Grocer::Notification.new(
       device_token:      device.token,
-      alert:             "#{user.first_name} wants to meet!",
+      alert:             message,
       badge:             1,
       expiry:            Time.now + 60*60    # optional; 0 is default, meaning the message is not stored
     )
+  end
 
+  def send_meetup_notification_to user
+    device = user.devices.last
+    message = "#{user.first_name} wants to meet!"
+    notification = generate_notification_for device, user, message
     pusher.push(notification)
   end
 end
