5481944e52949632c9b8933873788d1b94ee54ae
Marcelo Lebre <marcelo@dwitmee.com>
2015-02-21 23:42:04 +0000

    fixing accept meetup resource

--- app/api/v1/meetup_api.rb
+++ app/api/v1/meetup_api.rb
@@ -50,6 +50,8 @@ module V1
               meetup.status = 'accepted'
               if meetup.save
                 notify_acceptance(params[:friend_id].to_i)
+
+              else
                 error! 'Access Denied', 403
               end
             else
