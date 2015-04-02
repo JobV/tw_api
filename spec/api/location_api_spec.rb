e6f7f5ad62510c2cc511331b1b7ce14ab56ffad7
Marcelo Lebre <marcelo@dwitmee.com>
2015-04-02 13:53:08 +0100

    fixing location api tests

--- spec/api/location_api_spec.rb
+++ spec/api/location_api_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'post /api/v1/users/:id/location', type: :request do
+RSpec.describe 'post /api/v1/users/location', type: :request do
   let(:user) { create(:user) }
 
   context 'post with all params' do
     before do
-      post "/api/v1/users/#{user.id}/location", x: '52.440723',
-                                                y: '4.809657',
-                                                z: '0',
-                                                m: '0'
+      token = create(:api_key, access_token: "12345678", expires_at: Date.tomorrow, user_id: user.id)
+
+      post "/api/v1/users/location", x: '52.440723',
+                                     y: '4.809657',
+                                     z: '0',
+                                     m: '0',
+                                     token: token.access_token
     end
 
     it 'returns 201' do
@@ -30,7 +33,9 @@ RSpec.describe 'post /api/v1/users/:id/location', type: :request do
 
   context 'post with too little params' do
     before do
-      post "/api/v1/users/#{user.id}/location", x: "11"
+      token = create(:api_key, access_token: "12345678", expires_at: Date.tomorrow, user_id: user.id)
+
+      post "/api/v1/users/location", x: "11", token: token.access_token
     end
 
     it 'returns 500' do
@@ -50,9 +55,11 @@ end
 RSpec.describe 'get /api/v1/users/:id/location', type: :request do
   let(:user) { create(:user) }
   before do
+    token = create(:api_key, access_token: "12345678", expires_at: Date.tomorrow, user_id: user.id)
+
     user.locations.create(
     longlat: "POINT(52.440723 4.809657 0 0)")
-    get "/api/v1/users/#{user.id}/location"
+    get "/api/v1/users/#{user.id}/location", token: token.access_token
   end
 
   it 'returns 200' do
