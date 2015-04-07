f5670a84f6064640248963398451531e4aaff955
Marcelo Lebre <marcelo@dwitmee.com>
2015-04-07 10:37:01 +0100

    adding debug logs to notifications

--- app/helpers/apns_helper.rb
+++ app/helpers/apns_helper.rb
@@ -23,24 +23,28 @@ module ApnsHelper
   end
 
   def push_declined_notification(user, device)
+    Rails.logger.info "======== user declined meetup =========="
     message = "#{user.first_name} denied to meetup!"
     notification = generate_notification_for device, message, user.id, 3
     pusher.push(notification)
   end
 
   def push_termination_notification(user, device)
+    Rails.logger.info "======== user terminated meetup =========="
     message = "#{user.first_name} terminated the meetup!"
     notification = generate_notification_for device, message, user.id, 4
     pusher.push(notification)
   end
 
   def push_accepted_notification(user, device)
+    Rails.logger.info "======== user accepted meetup request =========="
     message = "#{user.first_name} accepted to meetup!"
     notification = generate_notification_for device, message, user.id, 2
     pusher.push(notification)
   end
 
   def push_request_notification(user, device)
+    Rails.logger.info "======== user wants to meetup =========="
     message = "#{user.first_name} wants to meet!"
     notification = generate_notification_for device, message, user.id, 1
     pusher.push(notification)
